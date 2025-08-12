6cb24122a4c9b8c722e7e12c4c448026de5b74f6
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-08-12 13:44:33 +0100

    feat: Edit email.
    Signed-off-by: ChibuikemLucas <chibuikemlucas@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("contact-btn").addEventListener("click", () => {
-    alert("Email me at: chibuikemlucas@email.com");
+    alert("Email me at: chibuikemlucas@gmail.com");
 });
+
